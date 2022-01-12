e95a0216107646c2a9cf92c0f4d3a6e68ea3eefc
Sevy Harris <harris.se@northeastern.edu>
2022-01-12 13:02:49 -0500

    remove settings yaml \n\nthe settings yaml is too bulky. I want to be able to copy and paste and adjust at the python level

--- basepys/bulk.py
+++ basepys/bulk.py
@@ -5,7 +5,6 @@
 import os
 from shutil import copyfile
 import sys
-import yaml
 from time import time
 from ase.build import bulk
 from ase.calculators.espresso import Espresso
@@ -13,64 +12,60 @@ from ase.calculators.socketio import SocketIOCalculator
 import socket
 
 start = time()
-# read in the settings
-settings_file = sys.argv[1]
-with open(settings_file) as f:
-    settings = yaml.load(f, Loader=yaml.FullLoader)
 
-BULK_DIR = settings['BULK_DIR']
-logfile = os.path.abspath(os.path.join(BULK_DIR, 'ase.log'))
+logfile = 'ase.log'
 
-# N_CPUS = 4
-# PW_EXEC = '/shared/centos7/q-e/q-e-qe-6.2.0-gcc7/bin/pw.x'
-# input_file = os.path.join(BASEDIR, 's1_bulk', 'bulk.pwi')
-# output_file = os.path.join(BASEDIR, 's1_bulk', 'bulk.pwo')
-# os.environ['ASE_ESPRESSO_COMMAND'] = f'mpirun -np {N_CPUS} {PW_EXEC} -in {input_file} > {output_file}'
-# os.environ['ASE_ESPRESSO_COMMAND'] = f'mpirun -np {N_CPUS} {PW_EXEC} -in PREFIX.pwi > PREFIX.pwo'
 
 metal = settings['metal']
-cu_bulk = bulk(
-    settings['metal'],
-    crystalstructure=settings['crystal_structure'],
-    a=settings['lattice_constant_guess'],
-    cubic=True
-)
+cu_bulk = bulk('Pt', 'fcc', a=4.0, cubic=True)
+espresso_settings = {
+    'control': {
+        'verbosity': 'high',
+        # 'disk_io': 'none',
+        'calculation': 'vc-relax',
+    },
+    'system': {
+        'input_dft': 'BEEF-VDW',
+        'occupations': 'smearing',
+        'degauss': 0.1,
+        'ecutwfc': 500,
+        # 'ecutrho': 500,
+    },
+    'ions': {
+        'ion_dynamics': 'bfgs',
+    },
+    'cell': {
+        'cell_dynamics': 'bfgs',
+        'press': 0.0,
+        'press_conv_thr': 0.005,
+    }
+}
 
-espresso_settings = settings['bulk_espresso_settings']
-espresso_settings['control']['forc_conv_thr'] = settings['forc_conv_thr_eVA'] / 51.42208619083232
+pseudopotentials = {
+    'C': 'C_ONCV_PBE-1.2.upf',
+    'Cu': 'Cu_ONCV_PBE-1.2.upf',
+    'O': 'O_ONCV_PBE-1.2.upf',
+    'Pt': 'Pt_ONCV_PBE-1.2.upf',
+}
 
-# remove everything that has 'default' as the value
-for category_key in espresso_settings.keys():
-    keys_to_remove = []
-    for setting_key in espresso_settings[category_key].keys():
-        if espresso_settings[category_key][setting_key] == 'default':
-            keys_to_remove.append(setting_key)
-    for key in keys_to_remove:
-        espresso_settings[category_key].pop(key)
-
-
-pw_executable = settings['pw_executable']
-port = 30141
-hostname = socket.gethostname()
-
+pw_executable = os.environ['PW_EXECUTABLE']
 espresso = Espresso(
-    command=f'{pw_executable} -in PREFIX.pwi --ipi {hostname}:{port} > PREFIX.pwo',
-    pseudopotentials=settings['pseudopotentials'],
+    command=f'mpirun -np 64 {pw_executable} -nimage 4 -npool 4 -in PREFIX.pwi > PREFIX.pwo',
+    pseudopotentials=pseudopotentials,
     tstress=True,
     tprnfor=True,
-    kpts=settings['kpts_bulk'],
-    pseudo_dir=settings['PSEUDOS_DIR'],
+    kpts=(16, 16, 16),
+    pseudo_dir=os.environ['PSEUDO_DIR'],
     input_data=espresso_settings,
     directory=BULK_DIR
 )
 
-with SocketIOCalculator(espresso, log=sys.stdout) as calc:
-    cu_bulk.calc = calc
-    cu_bulk.get_potential_energy()
+cu_bulk.calc = calc
+cu_bulk.get_potential_energy()
 
 
-# copy the file
-copyfile(os.path.abspath(os.path.join(BULK_DIR, 'espresso.pwo')), settings['BULK_FILE'])
+# copy the file so we don't accidentally overwrite it
+copyfile('espresso.pwo', 'bulk.pwo')
 
 end = time()
 duration = end - start
